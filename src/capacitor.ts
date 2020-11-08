266120305c3fe49b9204f4f7cbf1dbfb222b0bc0
bluehexagons <bluehexagons@users.noreply.github.com>
2020-11-08 10:26:32 -0600

    Minimally functioning release

--- src/capacitor.ts
+++ src/capacitor.ts
@@ -1,40 +1,90 @@
 export type Comparator<V> = (a: V, b: V) => boolean
 
-// const maxRollback = 0
-
 export class Client<V> {
+  /** How many frames to allow interpolation */
+  interpolate = 0
+  /** Size of buffer, minus interpolation */
+  size = 0
+
+  /** List of all commits */
   commits: (V | null)[] = []
-  lastReal = -1
+
+  /** Cache current frame state */
+  cache: V | null = null
+
+  // TODO: is it better to store the comparator as a property,
+  // or pass it from Capacitor to the commit method?
+  constructor(public comparator: Comparator<V>) { }
 
   /** Returns true if no error correction necessary */
-  commit(index: number, value: V, comparator: Comparator<V>): boolean {
-    while (this.commits.length < index - 1) {
+  commit(index: number, value: V): boolean {
+    // TODO: update size
+
+    while (this.commits.length < index + 1) {
       this.commits.push(null)
     }
 
     const before = this.commits[index]
 
     if (before !== null) {
-      const compare = comparator(before, value)
+      const compare = this.comparator(before, value)
 
       if (!compare) {
         this.commits[index] = value
+
         return false
       }
     }
 
     this.commits[index] = value
+
     return true
   }
 
   read(index: number): (V | null) {
+    // TODO: interpolation logic will go here
+    // TODO: swap length check for this.size + this.interpolate
     return this.commits.length > index ? this.commits[index] : null
   }
 }
 
 export class Capacitor<C, V> {
   commits: C[] = []
-  clients = new Map<number, Client<V>>()
+  clients = new Set<Client<V>>()
 
   equality: Comparator<V> = () => false
+
+  constructor(public comparator: Comparator<V>) { }
+
+  /** Create a new client */
+  connect() {
+    const client = new Client<V>(this.comparator)
+
+    this.clients.add(client)
+
+    return client
+  }
+
+  /**
+   * Updates client caches
+   * Returns true if all clients were updated, false if any were not
+   * Short-circuits on first failed client update
+   */
+  read(index: number) {
+    for (const client of this.clients) {
+      client.cache = client.read(index)
+
+      if (client.cache === null) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  /** Clears clients and commits */
+  clear() {
+    this.clients.clear()
+    this.commits = []
+  }
 }
--- test/capacitor.test.ts
+++ test/capacitor.test.ts
@@ -1,8 +1,110 @@
 import { Capacitor } from '../src/capacitor'
 
+interface State {
+  text: string
+}
+
+interface Packet {
+  value: number
+}
+
+const compare = (a: Packet, b: Packet) => a.value === b.value
+
 describe('usage', () => {
-  console.log(Capacitor)
-  test('yeah', () => {
-    expect(true).toBe(true)
+  test('client creation and use', () => {
+    const cap = new Capacitor<State, Packet>(compare)
+    const client = cap.connect()
+    let v = client.read(0)
+    expect(v).toBe(null)
+
+    let ok = client.commit(0, { value: 0 })
+    expect(ok).toBe(true)
+    v = client.read(0)
+    expect(v?.value).toBe(0)
+
+    ok = client.commit(0, { value: 1 })
+    expect(ok).toBe(false)
+    v = client.read(0)
+    expect(v?.value).toBe(1)
+
+    ok = client.commit(0, { value: 1 })
+    expect(ok).toBe(true)
+    v = client.read(0)
+    expect(v?.value).toBe(1)
+
+    ok = client.commit(1, { value: 0 })
+    expect(ok).toBe(true)
+    v = client.read(0)
+    expect(v?.value).toBe(1)
+    v = client.read(1)
+    expect(v?.value).toBe(0)
+    v = client.read(2)
+    expect(v).toBe(null)
+  })
+
+  test('server creation and use', () => {
+    const cap = new Capacitor<State, Packet>(compare)
+    const client = cap.connect()
+
+    let ok = cap.read(0)
+    expect(ok).toBe(false)
+    expect(client.cache).toBe(null)
+
+    ok = client.commit(1, { value: 0 })
+    expect(ok).toBe(true)
+
+    ok = cap.read(0)
+    expect(ok).toBe(false)
+    expect(client.cache).toBe(null)
+
+    // TODO: enable this test when future data is correctly disallowed
+    // ok = cap.read(1)
+    // expect(ok).toBe(false)
+
+    ok = client.commit(0, {value: 1})
+    expect(ok).toBe(true)
+    ok = cap.read(0)
+    expect(ok).toBe(true)
+    expect(client.cache?.value).toBe(1)
+
+    ok = cap.read(1)
+    expect(ok).toBe(true)
+    expect(client.cache?.value).toBe(0)
+
+    ok = cap.read(2)
+    expect(ok).toBe(false)
+
+    const client2 = cap.connect()
+    ok = cap.read(0)
+    expect(ok).toBe(false)
+
+    ok = client2.commit(0, {value: 0})
+    expect(ok).toBe(true)
+    ok = cap.read(0)
+    expect(ok).toBe(true)
+    expect(client.cache?.value).toBe(1)
+    expect(client2.cache?.value).toBe(0)
+
+    ok = cap.read(1)
+    expect(ok).toBe(false)
+
+    ok = client2.commit(1, {value: 1})
+    expect(ok).toBe(true)
+
+    const testSize = 16
+    for (let i = 1; i < testSize; i++) {
+      // client overwrites
+      expect(client.commit(i, {value: i - 1})).toBe(true)
+      expect(client2.commit(i, {value: i})).toBe(true)
+    }
+
+    ok = true
+    for (let i = 1; i < testSize; i++) {
+      expect(cap.read(i)).toBe(true)
+      expect(client.cache?.value).toBe(i - 1)
+      expect(client2.cache?.value).toBe(i)
+    }
+
+    expect(cap.read(testSize)).toBe(false)
   })
 })

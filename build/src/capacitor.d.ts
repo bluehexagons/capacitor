927bb25f1891f1f6834e64ff4ce98e42b16dd397
bluehexagons <loren@bluehexagons.com>
2026-03-24 10:46:04 -0500

    package(llm): support public git installs

---
+++ build/src/capacitor.d.ts
@@ -0,0 +1,31 @@
+export type Comparator<V> = (a: V, b: V) => boolean
+
+interface ClientProps<V> {
+  comparator?: Comparator<V>
+  sizeOffset?: number
+}
+
+export declare class Client<V> {
+  comparator: Comparator<V>
+  interpolate: number
+  size: number
+  sizeOffset: number
+  commits: (V | null)[]
+  cache: V | null
+  constructor({ comparator, sizeOffset }: ClientProps<V>)
+  commit(outerIndex: number, value: V): boolean
+  read(outerIndex: number): V | null
+}
+
+export declare class Capacitor<C, V> {
+  commits: C[]
+  clients: Set<Client<V>>
+  equality: Comparator<V>
+  comparator: Comparator<V>
+  constructor(comparator: Comparator<V>)
+  connect(props: ClientProps<V>): Client<V>
+  disconnect(client: Client<V>): void
+  read(index: number): boolean
+  clear(): void
+  size(): number
+}
